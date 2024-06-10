f08e65bea70216a601b0312fb532883f464ed2b0
Fabián Huertas <miloh9318@hotmail.com>
2024-06-09 19:03:54 -0500

    primeros pasos en editarcliente.js

---
+++ js/editarcliente.js
@@ -0,0 +1,8 @@
+(function () {
+    
+    document.addEventListener('DOMContentLoaded', () => {
+
+        // Verificar el ID de la URL
+        const parametrosURL = new URLSearchParams(Window.location.search);
+    });
+})();
